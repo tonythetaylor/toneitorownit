76aec4c9b77d9738b6a597daf0c5f71f68af5de0
Anthony Taylor <ant.taylor2@gmail.com>
2024-03-01 23:31:05 -0500

    updates for simple user sign in, search and pagination

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -6,7 +6,7 @@ import {
   ActivityIndicator,
 } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
-// import exercises from '../../assets/data/exercises.json';
+import exercises from '../../assets/data/exercises.json';
 import { Stack } from 'expo-router';
 import { useState } from 'react';
 import { gql } from 'graphql-request';
